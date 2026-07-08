bd8abf9ba35fc7fcf84fa4d250b77b6e57ecd273
Omar Khalil <o.khalil.viganium@gmail.com>
2026-07-08 12:26:17 +0300

    update: description

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -3,7 +3,7 @@ import "./globals.css";
 
 export const metadata: Metadata = {
   title: "Abdallah & Hoda",
-  description: "You're invited to celebrate the wedding of Mazen & Diana",
+  description: "You're invited to celebrate the wedding of Abdallah & Hoda",
 };
 
 export default function RootLayout({
